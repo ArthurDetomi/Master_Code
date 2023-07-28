bd3e0de5f02ffebd80e1dbddc8c1ce7c20fa6237
Arthur Detomi <arthurdetomi@gmail.com>
2023-07-28 17:16:33 -0300

    Add comentaries from implementation

--- Algorithms-Data-Structures/section6-arrays/leetcode_exercises/move_zeros.cpp
+++ Algorithms-Data-Structures/section6-arrays/leetcode_exercises/move_zeros.cpp
@@ -1,17 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Complexidade de tempo : O(n) 
+// Complexidade de memória : O(n)
+// Explicando como foi feito a implementação
 void moveZeroes(vector<int> &nums) {
     const int N = nums.size();
 
     queue<int> q;
 
+    // Copiando valores diferente de zero para fila
     for (int i = 0; i < N; i++) {
         if (nums[i] != 0) {
             q.push(nums[i]);
         }
     }
 
+    // Reordenando array consumindo a fila e movendo zeros para o final
     for (int i = 0; i < N; i++) {
         if (!q.empty()) {
             nums[i] = q.front();
@@ -23,5 +28,5 @@ void moveZeroes(vector<int> &nums) {
 }
 
 int main() {
-    // implement test in the future
+    // Implementação e teste no futuro
 }
