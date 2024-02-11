f6ba3d15eb89d741f37bb9bc1363765b09b821dd
Arthur Detomi <arthurdetomi@gmail.com>
2024-02-11 15:52:16 -0300

    Section 7 : Implementando pilha

---
+++ Algorithms-Data-Structures/section9-stack-queue/stack.js
@@ -0,0 +1,57 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+
+    peek() {
+        return this.top.value;
+    }
+
+    push(value) {
+        const sendValue = new Node(value);
+        if (this.length === 0) {
+            this.bottom = sendValue;
+            this.top = sendValue;
+        } else {
+            const holdPointer = this.top;
+            this.top = sendValue;
+            this.top.next = holdPointer;
+        }
+
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if (!this.top) {
+            return null;
+        } else if (this.top === this.bottom) {
+            this.bottom = null;
+        }
+        this.top = this.top.next;
+        this.length--;
+        return this;
+    }
+}
+
+let stack = new Stack();
+
+stack.push("Google");
+stack.push("Udemy");
+stack.push("Academy");
+
+
+for (let i = 0, length = stack.length; i < length; i++) {
+    let value = stack.peek();
+    stack.pop();
+    console.log(value);
+}
